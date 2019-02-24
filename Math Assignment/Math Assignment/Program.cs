1754288abe012e3895e31a8f2624bdfc027a38e9
Mads Kildegaard Kristjansen <m.i.s.s.mads@hotmail.com>
2019-02-24 15:04:08 +0100

    Assignment for Marco lesson 2

---
+++ Math Assignment/Math Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+            Console.WriteLine("Give me a number!");
+            string firstNumberLetter = Console.ReadLine();
+            int firstNumber = int.Parse(firstNumberLetter);
+     
+            Console.WriteLine("Good. Now give me another, or same, number!");
+            string secondNumberLetter = Console.ReadLine();
+            int secondNumber = int.Parse(secondNumberLetter);
+
+            if (firstNumber == secondNumber)
+            {
+                int newNumber = firstNumber + secondNumber;
+                int finalNumber = newNumber + newNumber + newNumber;
+                Console.WriteLine(finalNumber);
+            }
+            else
+            {
+                Console.WriteLine(firstNumber + secondNumber);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
